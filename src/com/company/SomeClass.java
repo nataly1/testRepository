4a4576a8c130aac6adcba92d416bc27517ca4570
Natasha <natalya_kolomec@mail.ru>
2017-11-08 21:06:12 +0700

    Initial commit.

---
+++ src/com/company/SomeClass.java
@@ -0,0 +1,12 @@
+package com.company;
+
+/**
+ * Created by Natasha on 20.10.2017.
+ */
+
+class SomeClass extends Main {
+    public void perform() {
+        super.perform();
+        System.out.print("2");
+    }
+}
